e855515eccd4d72522422836934eb4f6299d5618
Addison Schuhardt <aschuhardt@gmail.com>
2016-12-07 13:05:46 -0800

    renamed a folder and removed unnecessary usings

--- roguelike/modules/Interface/IModule.cs
+++ roguelike/modules/Interface/IModule.cs
@@ -1,9 +1,5 @@
 ﻿using roguelike.entity;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace roguelike.modules {
     interface IModule {
