a51e366eb947d8aed94ead8880d32eac67ab12b4
Addison Schuhardt <aschuhardt@gmail.com>
2016-12-07 15:15:46 -0800

    added interface + base for NPC class

---
+++ roguelike/modules/game_core/npc/base/NPCBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace roguelike.modules.game_core.npc {
+    class NPCBase : INPC {
+
+        private Guid _id;
+
+        public Guid id {
+            get {
+                return _id;
+            }
+        }
+
+        protected NPCBase() {
+            _id = Guid.NewGuid();
+        }
+    }
+}
