985dbc6251d912cf0846fa679e4b389e282c176b
Addison Schuhardt <aschuhardt@gmail.com>
2016-12-11 18:12:02 -0800

    fixing reference issues and cleaning up test app

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,21 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using RoguePanda;
-using System.Reflection;
+﻿using RoguePanda;
 
 namespace TestApp {
     class Program {
         static void Main(string[] args) {
-            Assembly a = Assembly.GetExecutingAssembly();
-
-            string s = a.FullName;
-
-            AssemblyName t = a.GetName();
-
-
             Game g = new Game();
             g.run();
         }
