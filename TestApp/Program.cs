e93219451e8ec1e51560cbff736a456b97afe420
Addison Schuhardt <aschuhardt@gmail.com>
2016-12-11 16:04:10 -0800

    added test app project

---
+++ TestApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RoguePanda;
+
+namespace TestApp {
+    class Program {
+        static void Main(string[] args) {
+            Game g = new Game();
+
+            g.run();
+        }
+    }
+}
