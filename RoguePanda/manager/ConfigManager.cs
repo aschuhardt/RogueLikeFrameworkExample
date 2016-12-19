bf23ed5ab83e15af50230da065b80f04d09b91cc
Addison Schuhardt <aschuhardt@gmail.com>
2016-12-19 13:42:17 -0800

    added fps profiler to test app

--- RoguePanda/manager/ConfigManager.cs
+++ RoguePanda/manager/ConfigManager.cs
@@ -22,7 +22,7 @@ namespace RoguePanda.manager {
 
         public static Config Config {
             get {
-                return ConfigManager.Config;
+                return Instance.Configuration;
             }
         }
 
